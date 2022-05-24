9f747696f5d8e8b5d057172f8ad3e05aa547786c
ShawnXiong <ShawnXiong@galaxy3dvision.com>
2022-05-24 00:06:47 -0700

    Fix ',' break

--- XPython.cpp
+++ XPython.cpp
@@ -11,13 +11,16 @@ static std::vector<short> _kwTree;
 void RunCore()
 {
 	std::string code =
-		"k=1\
+		"k=1.2345\
 		st='this is \"a\" string'\
 		s0 =\"another strng\"\
 		x=1\
-		def func1(ddd:str,100:int):\
+		def func1(ddd:str,i1:int):\
 			y=2\
-			z=x+y";
+			z=x+y\
+		var1 = func1(st,100)";
+	std::string code0 = "var1 = func1(st,100)";
+
 	PyInit(&_kwTree[0]);
 
 	PyHandle h = PyLoad((char*)code.c_str(), (int)code.size());
