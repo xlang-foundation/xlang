70ac7a4e106b9ebd701b49901dfe866b4c1c0b9f
shawn <shawnxiong@galaxy3dvision.com>
2022-08-28 00:24:56 -0700

    Yaml

--- Text/yaml_parser.cpp
+++ Text/yaml_parser.cpp
@@ -1,62 +1,359 @@
 #include "yaml_parser.h"
-#include "lex.h"
 #include <iostream>
+
+//https://yaml.org/spec/1.1/#id857181
 
 namespace X
 {
 	namespace Text
 	{
-		std::vector<OpInfo> YamlParser::OPList =
-		{
-			{0,":"},{1,","},
-			{2,"{"},{3,"}"},
-			{4,"["},{5,"]"},
-			{6,"---"},{7,"-"}
-		};
-		std::vector<short> YamlParser::kwTree;
-		std::vector<OpAction> YamlParser::OpActions;
-
 		YamlParser::YamlParser()
 		{
 		}
 
 		YamlParser::~YamlParser()
 		{
-			if (mToken)
-			{
-				delete mToken;
-			}
+
 		}
 
 		bool YamlParser::Init()
 		{
-			Lex<OpInfo, OpAction>().MakeLexTree(
-				OPList, kwTree, OpActions);
-			mToken = new Token(&kwTree[0]);
 			return true;
 		}
 		bool YamlParser::LoadFromString(char* code, int size)
 		{
-			mToken->SetStream(code, size);
-			return Parse();
+			m_data = code;
+			m_size = size;
+			return true;
+		}
+		void YamlParser::Process()
+		{
+			while (m_bRun)
+			{
+				auto it0 = m_events.begin();
+				auto evt = *it0;
+				m_events.erase(it0);
+				EventHandler handler = m_Handlers[evt];
+				try
+				{
+					handler(0);
+				}
+				catch (eventType e) 
+				{
+					std::cout << (int)e << std::endl;
+					Fire(e);
+				}
+			}
+		}
+		YamlNode* YamlParser::GetParentNode(YamlNode* pCurNode, Status& status)
+		{//for sequence
+			YamlNode* pMyParentNode = pCurNode;
+			while (pMyParentNode)
+			{
+				if (status.LeadingSpaces == pMyParentNode->LeadingSpaces())
+				{
+					pMyParentNode = pMyParentNode->Parent();
+					break;
+				}
+				else if (status.LeadingSpaces < pMyParentNode->LeadingSpaces())
+				{
+					pMyParentNode = pMyParentNode->Parent();
+				}
+				else
+				{//greater
+					break;
+				}
+			}
+			return pMyParentNode;
+		}
+		YamlNode* YamlParser::GetParentNode2(YamlNode* pCurNode, Status& status)
+		{//for sequence
+			YamlNode* pMyParentNode = pCurNode;
+			//check if pCurNode is a value node
+			if (IsValueNode(pCurNode))
+			{
+				pMyParentNode = pCurNode;
+			}
+			else
+			{
+				while (pMyParentNode)
+				{
+					if (status.LeadingSpaces == pMyParentNode->LeadingSpaces())
+					{
+						pMyParentNode = pMyParentNode->Parent();
+						break;
+					}
+					else if (status.LeadingSpaces < pMyParentNode->LeadingSpaces())
+					{
+						pMyParentNode = pMyParentNode->Parent();
+						if (pMyParentNode && IsValueNode(pMyParentNode))
+						{
+							pMyParentNode = pMyParentNode->Parent();
+						}
+					}
+					else
+					{//greater
+						break;
+					}
+				}
+			}
+			return pMyParentNode;
+		}
+		bool YamlParser::IsValueNode(YamlNode* pNode)
+		{
+			return (pNode->Parent() &&
+				pNode->Parent()->ValueNode() == pNode);
+		}
+		YamlNode* YamlParser::GetParentNode3(YamlNode* pCurNode, Status& status)
+		{
+			YamlNode* pMyParentNode = pCurNode;
+			if (IsValueNode(pMyParentNode))
+			{
+				//compair wtih the main key part's LeadingSpace
+				YamlNode* pKeyNode = pMyParentNode->Parent();
+				if (status.LeadingSpaces == pKeyNode->LeadingSpaces())
+				{
+					pMyParentNode = pKeyNode->Parent();
+					return pMyParentNode;
+				}
+				else if(status.LeadingSpaces > pKeyNode->LeadingSpaces())
+				{
+					return pCurNode;
+				}
+			}
+			if (status.LeadingSpaces <= pMyParentNode->LeadingSpaces())
+			{
+				pMyParentNode = pMyParentNode->Parent();
+				if (IsValueNode(pMyParentNode))
+				{
+					pMyParentNode = pMyParentNode->Parent();
+				}
+				while (pMyParentNode &&
+					status.LeadingSpaces <= pMyParentNode->LeadingSpaces())
+				{
+					pMyParentNode = pMyParentNode->Parent();
+					if (pMyParentNode && IsValueNode(pMyParentNode))
+					{
+						pMyParentNode = pMyParentNode->Parent();
+					}
+				}
+			}
+			return pMyParentNode;
+		}
+		YamlNode* YamlParser::Scan(YamlNode* pCurNode, Status& status)
+		{
+			char ch = getChar();
+			auto start = getPos();
+			if (status.NewLine)
+			{//Count leading space
+				while (ch == ' ' || ch == '\t')
+				{
+					if (ch == ' ')
+					{
+						status.LeadingSpaces += 1;
+					}
+					else
+					{
+						status.LeadingTabs += 1;
+					}
+					ch = getChar();
+				}
+			}
+			if (ch == '-' && status.NewLine)
+			{
+				ch = getChar();
+				if (ch == ' ')
+				{
+					start = getPos();
+					YamlNode* pMyParentNode = GetParentNode(pCurNode, status);
+					//new sequence node
+					YamlNode* pNewNode = new YamlNode(start,status.lineNo);
+					pNewNode->SetLeadingInfo(status.LeadingSpaces,
+						status.LeadingTabs);
+					pNewNode->SetType(YamlNodeType::Sequence);
+					pNewNode->SetParent(pMyParentNode);
+					pMyParentNode->Add(pNewNode);
+					pCurNode = pNewNode;
+				}
+				else if (ch == '-')
+				{
+					ch = getChar();
+					if (ch == '-')
+					{
+						if (pCurNode
+							&& pCurNode->Type() == YamlNodeType::Doc
+							&& pCurNode->IsNullStartPos())
+						{
+							pCurNode->SetStartPos(start,status.lineNo);
+						}
+						else
+						{//new document
+							pCurNode = new YamlNode(start, status.lineNo);
+							pCurNode->SetLeadingInfo(status.LeadingSpaces,
+								status.LeadingTabs);
+							pCurNode->SetType(YamlNodeType::Doc);
+						}
+					}
+					else
+					{
+						//treat as Key starts from var:start
+						YamlNode* pNewNode = new YamlNode(start, status.lineNo);
+						pNewNode->SetLeadingInfo(status.LeadingSpaces,
+							status.LeadingTabs);
+						pNewNode->SetType(YamlNodeType::Dict);
+						pCurNode->Add(pNewNode);
+						pCurNode = pNewNode;
+					}
+				}
+				else
+				{
+					//treat as Key starts from var:start
+					YamlNode* pNewNode = new YamlNode(start, status.lineNo);
+					pNewNode->SetLeadingInfo(status.LeadingSpaces,
+						status.LeadingTabs);
+					pNewNode->SetType(YamlNodeType::Dict);
+					pCurNode->Add(pNewNode);
+					pCurNode = pNewNode;
+				}
+			}
+			else if(ch ==':')
+			{
+				pCurNode->SetEndPos(start-1, status.lineNo);
+				start = getPos();
+				YamlNode* pNewNode = new YamlNode(start, status.lineNo);
+				pCurNode->SetValueNode(pNewNode);
+				//still use pCurNode as Current Node
+				//pCurNode = pNewNode;
+			}
+			else if (ch == '\n')
+			{//end line
+				pCurNode->SetEndPos(start-1, status.lineNo);
+				status.NewLine = true;
+				status.LeadingSpaces = 0;
+				status.LeadingTabs = 0;
+				status.lineNo += 1;
+			}
+			else
+			{
+				//Reset line
+				status.NewLine = false;
+				if (pCurNode->IsClosed())
+				{
+					YamlNode* pMyParentNode = GetParentNode(pCurNode,status);
+					YamlNode* pNewNode = new YamlNode(start, status.lineNo);
+					pNewNode->SetParent(pMyParentNode);
+					pNewNode->SetLeadingInfo(status.LeadingSpaces,
+						status.LeadingTabs);
+					pMyParentNode->Add(pNewNode);
+					pCurNode = pNewNode;
+				}
+			}
+			return pCurNode;
 		}
 		bool YamlParser::Parse()
 		{
-			while (true)
+			m_cur = m_data;
+			m_last = m_data + m_size;
+			m_bRun = true;
+			Status status;
+			status.NewLine = true;
+			YamlNode* pRootNode = nullptr;
+			YamlNode* pCurNode = nullptr;
+			while (m_bRun)
 			{
-				String s;
-				int leadingSpaceCnt = 0;
-				OneToken one;
-				short idx = mToken->Get(one);
-				int startLine = one.lineStart;
-				s = one.id;
-				if (idx == TokenEOS)
+				try
 				{
+					pCurNode = Scan(pCurNode, status);
+				}
+				catch (eventType e)
+				{
+					std::cout << (int)e << std::endl;
 					break;
 				}
-				std::string txt(s.s, s.size);
-				std::cout << txt << ",idx:" << idx << ",line:"
-					<< one.lineStart << ",pos:" << one.charPos << std::endl;
+				if (pRootNode == nullptr)
+				{
+					pRootNode = pCurNode;
+				}
+			}
+			return true;
+		}
+		bool YamlParser::Parse3()
+		{
+			On(eventType::Start, [this](int x)
+			{
+				getChar();
+			});
+			On(eventType::End, [this](int x)
+			{
+					m_bRun = false;
+			});
+			//init
+			m_cur = m_data;
+			//m_last = m_data + m_size;
+			m_bRun = true;
+			Fire(eventType::Start);
+			Process();
+			return true;
+		}
+		bool YamlParser::Parse2()
+		{
+			struct statusInfo
+			{
+				
+			};
+			char* start = m_data;
+			char* end = m_data + m_size;
+			while (start<end)
+			{
+				char ch = *start++;
+				//feed space at line begin
+				int space_cnt = 0;
+				while (ch == ' ' && start<end)
+				{
+					space_cnt++;
+					ch = *start++;
+				}
+				if (ch == '-')
+				{
+					if (space_cnt == 0)
+					{
+						if (start < (end - 2) && *start =='-' && *(start+1) =='-')
+						{
+							//document start
+							start += 2;
+							continue;
+						}
+					}
+					if (start < end && *start == ' ')
+					{
+						//enter sequence
+						start++;
+						continue;
+					}
+				}
+				if (ch == ':')
+				{
+					if (start < end && *start == ' ')
+					{
+						//meet common case
+						start++;
+						//process chars before
+
+						continue;
+					}
+				}
+				if (ch == '"' || ch == '\'')
+				{
+
+				}
+				if (ch == '#')
+				{
+
+				}
+				if (ch == '\n')
+				{
+
+				}
 			}
 			return true;
 		}
--- Text/yaml_parser.h
+++ Text/yaml_parser.h
@@ -1,24 +1,163 @@
 #pragma once
 
-#include "token.h"
-#include "def.h"
-
+#include <functional>
+#include <unordered_map>
+#include <vector>
 namespace X
 {
 	namespace Text
 	{
+		enum class YamlNodeType
+		{
+			Node,
+			Doc,
+			Sequence,
+			Dict
+		};
+		class YamlNode
+		{
+		protected:
+			char* m_start_pos =nullptr;
+			char* m_end_pos = nullptr;
+			int m_startLineNo = 0;
+			int m_endLineNo = 0;
+			std::vector<YamlNode*> m_children;
+			YamlNode* m_parent = nullptr;
+			YamlNode* m_valueNode = nullptr;//for dict node
+			YamlNodeType m_type = YamlNodeType::Node;
+
+			int m_leadingSpaces = 0;
+			int m_leadingTabs = 0;
+		public:
+			YamlNode(char* startPos, int lineNo)
+			{
+				m_start_pos = startPos;
+				m_startLineNo = lineNo;
+			}
+			void SetStartPos(char* p,int lineNo)
+			{
+				m_start_pos = p;
+				m_startLineNo = lineNo;
+			}
+			void SetEndPos(char* p, int lineNo)
+			{
+				if(m_valueNode)
+				{ 
+					m_valueNode->SetEndPos(p, lineNo);
+				}
+				else
+				{
+					m_end_pos = p;
+					m_endLineNo = lineNo;
+				}
+			}
+			void SetLeadingInfo(int spaces, int tabs)
+			{
+				m_leadingSpaces = spaces;
+				m_leadingTabs = tabs;
+			}
+			int LeadingSpaces() { return m_leadingSpaces; }
+			bool IsClosed()
+			{
+				if (m_valueNode)
+				{
+					return m_valueNode->IsClosed();
+				}
+				else
+				{
+					return m_end_pos != nullptr;
+				}
+			}
+			void SetType(YamlNodeType t)
+			{
+				m_type = t;
+			}
+			void SetParent(YamlNode* pNode)
+			{
+				m_parent = pNode;
+			}
+			void SetValueNode(YamlNode* pNode)
+			{
+				m_valueNode = pNode;
+				m_valueNode->SetParent(this);
+			}
+			YamlNode* Parent() { return m_parent; }
+			YamlNode* ValueNode() { return m_valueNode; }
+			void Add(YamlNode* pNode)
+			{
+				if (m_valueNode)
+				{
+					m_valueNode->Add(pNode);
+				}
+				else
+				{
+					m_children.push_back(pNode);
+				}
+			}
+			YamlNodeType Type() { return m_type; }
+			bool IsNullStartPos() { return m_start_pos == nullptr; }
+		};
+
+		struct Status
+		{
+			bool NewLine = false;
+			int lineNo = 1;
+			int LeadingSpaces = 0;
+			int LeadingTabs = 0;
+		};
 		class YamlParser
 		{
-			Token* mToken = nullptr;
-			static std::vector<OpInfo> OPList;
-			static std::vector<short> kwTree;
-			static std::vector<OpAction> OpActions;
+			typedef std::function<void(int)> EventHandler;
+			enum class eventType
+			{
+				Start,
+				New_Sequence,
+				End
+			};
+			char* m_data = nullptr;
+			int m_size = 0;
+			char* m_cur = nullptr;
+			char* m_last = nullptr;
+			std::unordered_map<eventType, EventHandler> m_Handlers;
+			std::vector<eventType> m_events;
+			void Fire(eventType evt)
+			{
+				m_events.push_back(evt);
+			}
+			void On(eventType evt, EventHandler func)
+			{
+				m_Handlers.emplace(std::make_pair(evt,func));
+			}
+			void Process();
+			bool m_bRun = false;
+
+			char getChar()
+			{
+				char ch = 0;
+				if (m_cur < m_last)
+				{
+					ch = *m_cur++;
+				}
+				else
+				{
+					throw eventType::End;
+				}
+				return ch;
+			}
+			char* getPos() { return m_cur; }
+			YamlNode* Scan(YamlNode* pCurNode, Status& status);
+			YamlNode* GetParentNode(YamlNode* pCurNode, Status& status);
+			YamlNode* GetParentNode3(YamlNode* pCurNode, Status& status);
+			YamlNode* GetParentNode2(YamlNode* pCurNode, Status& status);
+			bool IsValueNode(YamlNode* pNode);
 		public:
 			YamlParser();
 			~YamlParser();
 			bool Init();
 			bool LoadFromString(char* code, int size);
 			bool Parse();
+			bool Parse3();
+			bool Parse2();
 		};
 	}
 }
