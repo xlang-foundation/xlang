cfce06e39c6456c25118998d3faaefdc20280b37
Shawn <shawn@cantorai.com>
2023-08-22 22:03:48 -0700

    http request

--- WebCore/HttpClient.h
+++ WebCore/HttpClient.h
@@ -15,11 +15,13 @@ namespace X
             BEGIN_PACKAGE(HttpRequest)
                 APISET().AddFunc<2>("setHeader", &HttpRequest::setHeader);
                 APISET().AddFunc<1>("sendRequest", &HttpRequest::sendRequest);
+                APISET().AddFunc<0>("readResponse", &HttpRequest::readResponse);
             END_PACKAGE
                 HttpRequest(const std::string url);
             ~HttpRequest();
             bool setHeader(const std::string name, const std::string value);
-            std::string sendRequest(const std::string body_content);
+            bool sendRequest(const std::string body_content);
+            X::Value readResponse();
         };
     }
 }
---
+++ WebCore/readme.md
@@ -0,0 +1,9 @@
+# Use Boost for websocket
+
+# but for http client
+- if use boost, will introduce to link to context libs  
+hate on that, which will bring a lot of dll for runtime
+
+- in windows, so directly use windows HTTP API
+- for Linux, maybe use libCurl
+ 
