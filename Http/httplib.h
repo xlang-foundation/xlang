0495448518e168ccecb2e203d6b46c8a0c3e3f8e
Galaxy <81903380+OliviaGalaxy3DVision@users.noreply.github.com>
2024-10-04 03:20:39 -0700

    https

--- Http/httplib.h
+++ Http/httplib.h
@@ -9362,7 +9362,7 @@ inline bool SSLClient::initialize_ssl(Socket &socket, Error &error) {
               return false;
             }
 
-            auto server_cert = SSL_get_peer_certificate(ssl2);
+            auto server_cert = SSL_get1_peer_certificate(ssl2);
             auto se = detail::scope_exit([&] { X509_free(server_cert); });
 
             if (server_cert == nullptr) {
