d9cb2cab5a81427e5ac54c2bb4f02fd41f2f6614
xlangdev <xlang@xlangfoundation.org>
2025-10-30 14:07:11 -0700

    using nlohmann/json replace xlang json

--- Text/json.h
+++ Text/json.h
@@ -4,7 +4,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,33 +17,45 @@ limitations under the License.
 
 #include "xpackage.h"
 #include "xlang.h"
+#include <nlohmann/json.hpp>
 
-namespace X 
+namespace X
 {
 	class JsonWrapper
 	{
 	public:
 		BEGIN_PACKAGE(JsonWrapper)
 			APISET().AddVarFunc("loads", &JsonWrapper::LoadFromString);
-			APISET().AddRTFunc<1>("load", &JsonWrapper::LoadFromFile);
-			APISET().AddVarFunc("saves", &JsonWrapper::SaveToString);
-			APISET().AddVarFunc("save", &JsonWrapper::SaveToFile);
-			APISET().AddVarFunc("dumps", &JsonWrapper::SaveToString);
-			APISET().AddVarFunc("dump", &JsonWrapper::SaveToFile);
-			END_PACKAGE
+		APISET().AddRTFunc<1>("load", &JsonWrapper::LoadFromFile);
+		APISET().AddVarFunc("saves", &JsonWrapper::SaveToString);
+		APISET().AddVarFunc("save", &JsonWrapper::SaveToFile);
+		APISET().AddVarFunc("dumps", &JsonWrapper::SaveToString);
+		APISET().AddVarFunc("dump", &JsonWrapper::SaveToFile);
+		END_PACKAGE
 	public:
 		JsonWrapper()
 		{
 		}
+
+		~JsonWrapper() = default;
+
+		// Load functions
+		X::Value LoadFromString(X::XRuntime* rt, X::XObj* pContext,
+			X::ARGS& params, X::KWARGS& kwParams, X::Value& retValue);
+		X::Value LoadFromFile(X::XRuntime* rt, X::XObj* pContext, std::string fileName);
+
+		// Save functions
 		bool SaveToString(X::XRuntime* rt, X::XObj* pContext,
 			X::ARGS& params, X::KWARGS& kwParams, X::Value& retValue);
 		bool SaveToFile(X::XRuntime* rt, X::XObj* pContext,
 			X::ARGS& params, X::KWARGS& kwParams, X::Value& retValue);
-		X::Value LoadFromString(X::XRuntime* rt, X::XObj* pContext,
-			X::ARGS& params, X::KWARGS& kwParams, X::Value& retValue);
-		X::Value  LoadFromFile(X::XRuntime* rt, X::XObj* pContext,std::string fileName);
+
 	private:
-		std::string ConvertXValueToJsonString(X::Value value,
-			bool prettyPrint, int indentLevel);
+		// Conversion functions between nlohmann::json and X::Value
+		X::Value ConvertJsonToXValue(const nlohmann::json& j);
+		nlohmann::json ConvertXValueToJson(X::Value& value);
+
+		// Helper for string normalization
+		std::string NormalizeJsonString(const std::string& jsonStr);
 	};
 }
