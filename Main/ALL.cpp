b0e00e652f7eba75e8ed73d5a316b799463b201d
xlangdev <xlang@xlangfoundation.org>
2026-01-15 10:33:28 -0800

    Fix case-sensitivity for Linux: Ast/Core/Api paths

--- Main/ALL.cpp
+++ Main/ALL.cpp
@@ -1,5 +1,5 @@
 
-#include "../ast/ALL_AST.cpp"
-#include "../core/ALL_CORE.cpp"
-#include "../api/value.cpp"
+#include "../Ast/ALL_AST.cpp"
+#include "../Core/ALL_CORE.cpp"
+#include "../Api/value.cpp"
 #include "../Parse/ALL_PARSE.cpp"
